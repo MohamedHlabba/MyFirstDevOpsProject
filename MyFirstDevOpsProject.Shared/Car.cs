e3ac2130032dfc2c65ca1c3b6ca84f2715295557
MohamedHlabba <mohamed.hlabba@esprit.tn>
2021-06-09 15:45:45 +0200

    Add CarImage

--- MyFirstDevOpsProject.Shared/Car.cs
+++ MyFirstDevOpsProject.Shared/Car.cs
@@ -4,11 +4,13 @@ namespace MyFirstDevOpsProject.Shared
 {
     public class Car
     {
-        public int CarId { get; set; }
+        public string Id { get; set; }
         public string Name { get; set; }
         public string Brand { get; set; }
         public string Model { get; set; }
-        public DateTime Year { get; set; }
+        public string ImageUrl { get; set; }
+
+        public string Year { get; set; }
         public Category Category { get; set; }
         public int CategoryId { get; set; }
         public string Comment { get; set; }
--- MyFirstDevOpsProject/Pages/CarResume.cs
+++ MyFirstDevOpsProject/Pages/CarResume.cs
@@ -1,69 +1,75 @@
-﻿using MyFirstDevOpsProject.Shared;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿//using Microsoft.AspNetCore.Components;
+//using MyFirstDevOpsProject.Shared;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Net.Http;
+//using System.Net.Http.Json;
+//using System.Threading.Tasks;
 
-namespace MyFirstDevOpsProject.Pages
-{
-    public  partial class CarResume
-    {
-        protected override Task OnInitializedAsync()
-        {
-            InitializeCars();
-            InitializeCategories();
-            return base.OnInitializedAsync();
-        }
-
-        public IEnumerable<Car> Cars { get; set; }
-
-        private List<Category> Categories { get; set; }
+//namespace MyFirstDevOpsProject.Pages
+//{
+//    public partial class CarResume
+//    {
+//        //private HttpClient client;
+//        //protected override async Task OnInitializedAsync()
+//        //{
 
 
-        private void InitializeCategories()
-        {
-            Categories = new List<Category>()
-            {
-                new Category{CategoryId = 1, CategoryName = "Sport"},
-                new Category{CategoryId = 2, CategoryName = "Coupe"},
-                new Category{CategoryId = 3, CategoryName = "4*4"},
-                new Category{CategoryId = 4, CategoryName = "Beetle"},
-                new Category{CategoryId = 5, CategoryName = "Convertible"},
-                new Category{CategoryId = 6, CategoryName = "Electric"},
-                new Category{CategoryId = 7, CategoryName = "Sedan"},
-                new Category{CategoryId = 8, CategoryName = "MiniVan"},
-                new Category{CategoryId = 9, CategoryName = "Muscle Car"},
-                new Category{CategoryId = 9, CategoryName = "Antique"},
-                new Category{CategoryId = 10, CategoryName = "Micro"}
-            };
-        }
+//        //    Cars = new List<Car>();
+//        //    Cars = await client.GetFromJsonAsync<IEnumerable<Car>>("https://dbuser13-funcapp.azurewebsites.net/api/cars");
+//        //}
+//       // public IEnumerable<Car> Cars { get; set; }
 
 
 
-        private void InitializeCars()
-        {
-            var c1 = new Car
-            {
-                CarId=1,
-                Brand = "BMW",
-                Name = "M3",
-                Model = "335 i",
-                CategoryId = 1,
-                Comment = "Lorem Ipsum",
-                Year = new DateTime(2015, 3, 1)
-            };
+//        private List<Category> Categories { get; set; }
 
-            var c2 = new Car
-            {
-                CarId=2,
-                Brand = "Mercedes",
-                Name = "SL500",
-                Model = "Class s AMG",
-                CategoryId = 1,
-                Comment = "Lorem Ipsum",
-                Year = new DateTime(2017, 12, 24)
-            };
-            Cars = new List<Car> { c1, c2 };
-        }
-    }
-}
+
+//        private void InitializeCategories()
+//        {
+//            Categories = new List<Category>()
+//            {
+//                new Category{CategoryId = 1, CategoryName = "Sport"},
+//                new Category{CategoryId = 2, CategoryName = "Coupe"},
+//                new Category{CategoryId = 3, CategoryName = "4*4"},
+//                new Category{CategoryId = 4, CategoryName = "Beetle"},
+//                new Category{CategoryId = 5, CategoryName = "Convertible"},
+//                new Category{CategoryId = 6, CategoryName = "Electric"},
+//                new Category{CategoryId = 7, CategoryName = "Sedan"},
+//                new Category{CategoryId = 8, CategoryName = "MiniVan"},
+//                new Category{CategoryId = 9, CategoryName = "Muscle Car"},
+//                new Category{CategoryId = 9, CategoryName = "Antique"},
+//                new Category{CategoryId = 10, CategoryName = "Micro"}
+//            };
+//        }
+
+
+
+//        //private void InitializeCars()
+//        //{
+//        //    var c1 = new Car
+//        //    {
+//        //        CarId = 1,
+//        //        Brand = "BMW",
+//        //        Name = "M3",
+//        //        Model = "335 i",
+//        //        CategoryId = 1,
+//        //        Comment = "Lorem Ipsum",
+//        //        Year = new DateTime(2015, 3, 1)
+//        //    };
+
+//        //    var c2 = new Car
+//        //    {
+//        //        CarId = 2,
+//        //        Brand = "Mercedes",
+//        //        Name = "SL500",
+//        //        Model = "Class s AMG",
+//        //        CategoryId = 1,
+//        //        Comment = "Lorem Ipsum",
+//        //        Year = new DateTime(2017, 12, 24)
+//        //    };
+//        //    Cars = new List<Car> { c1, c2 };
+//        //}
+//    }
+//}
